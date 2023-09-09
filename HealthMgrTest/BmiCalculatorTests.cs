8e4d57d5835ef947aaa83f6a1e478336909d5033
Eddie_Chen <bda605@hotmail.com>
2023-09-09 08:42:36 +0800

    加入專案檔案。

---
+++ HealthMgrTest/BmiCalculatorTests.cs
@@ -0,0 +1,22 @@
+namespace HealthMgrTest
+{
+    public class BmiCalculatorTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CalculateTest()
+        {
+            HealthMgr.BmiCalculator bmi = new HealthMgr.BmiCalculator();
+            bmi.Height = 170;
+            bmi.Weight = 70;
+
+            var result = bmi.Calculate();
+
+            Assert.AreEqual("24.22", result.ToString("00.00"));
+        }
+    }
+}
---
+++ WebBMI/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebBMI.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public float BmiResult = 0;
+
+        [BindProperty]
+        public int fieldHeight { get; set; }
+        [BindProperty]
+        public int fieldWeight { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPostCalculate()
+        {
+            HealthMgr.BmiCalculator bc = new HealthMgr.BmiCalculator();
+
+            bc.Height = fieldHeight;
+            bc.Weight = fieldWeight;
+
+            BmiResult = bc.Calculate();
+        }
+
+    }
+}
